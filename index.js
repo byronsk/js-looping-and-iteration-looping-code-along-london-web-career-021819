8b9c8918e8663b672975e909c2be9f69e53c21fb
byronsk <vyskoutaris@hotmail.com>
2019-04-03 16:14:58 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
+  }
+  return names
+}
+
+
+function tailsNeverFails() {
+  function flipcoin() {
+    return Math.random() >= 0.5
+  }
+  let num_flips = 0
+  while (flipcoin()) {
+    num_flips += 1
+  }
+  return `You got ${num_flips} tails in a row!`
+}
